b6300b2d1d906fd0b7d3fc63c56319e5167f0257
aikawa-joji <j.aikawa.sys19@morijyobi.ac.jp>
2020-06-29 11:22:35 +0900

    aa

---
+++ update.php
@@ -0,0 +1,25 @@
+<?php
+    $dsn = 'mysql:dbname=sample_db;host=localhost;';
+    $user = 'aikawa';
+    $password = 'aijo0907';
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(POD::ATTR_ERROMDE, PDO::ERRMODE_EXCEPTION;)
+
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+​
+        $sql = "update user set name=:name age=:age where id = :id";
+        $stmt = $dbh->prepare($sql);
+        $prams = array(':id'=> $id, ':name'=> $name, ':age'=> $age,);
+        $stmt->execute($prams);
+
+        header('Location: index.php?fg=1');
+
+    } catch (PDOException $e) {
+        header('Location: index.php?fg=2?err='.$e->getMessage());
+        exit();
+    }
+​
+?>
